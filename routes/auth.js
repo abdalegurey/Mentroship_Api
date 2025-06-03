b7dfa12db6f66aa745db599769953aee71c391ba
abdalegurey <[200~abdalegureyguled@gmail.com~>
2025-06-03 09:09:42 +0300

    adding swagger

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,33 @@ import { createUserSchema } from "../Schemas/UserSchema.js";
 import { validate } from "../Middlewares/validateZod.js";
 const router = express.Router();
 
-
+/**
+ * @swagger
+ * /auth/Register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User registered
+ */
 
 
 router.post('/Register', validate(createUserSchema), registerUser);
