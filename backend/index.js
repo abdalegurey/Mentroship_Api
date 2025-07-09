e03742c5be4cb6610c7d607fe73eaa18d83aacb7
abdalegurey <[200~abdalegureyguled@gmail.com~>
2025-07-09 21:57:11 +0300

    added url

--- backend/index.js
+++ backend/index.js
@@ -137,6 +137,13 @@ app.get('/api/health', (req, res) => {
 
 
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 
 app.use(Notfound);
