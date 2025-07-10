da09f770e4cb199db358610864e80513b5273ada
abdalegurey <[200~abdalegureyguled@gmail.com~>
2025-07-10 10:50:35 +0300

    added new feature

--- backend/index.js
+++ backend/index.js
@@ -141,12 +141,12 @@ app.get('/api/health', (req, res) => {
 
 
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// const __dirname = path.resolve();
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// });
 
 
 
